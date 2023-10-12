59cd6f29220b51c24ef2d3398cb1965931bcd71b
Yasaswini-V <145005383+Yasaswini-V@users.noreply.github.com>
2023-10-12 21:40:17 +0530

    Update Users.py

--- App1/Users.py
+++ App1/Users.py
@@ -7,7 +7,7 @@ def get_all_users(cursor):
             print(res)
     
 def add_new_user(cursor,id,name,email):
-    cursor.execute('insert into Users (User_ID,Name,Email) values (?,?,?)',id,name,email)
+    cursor.execute('insert into Users (userid,username,password) values (?,?,?)',id,name,password)
 
 def get_user_review(cursor,id):
     cursor.execute('''SELECT R.userid,B.title, R.rating, R.review_text
